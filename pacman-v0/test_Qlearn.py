3dc6746d60e7fb84ce267e6991b6152f1939d0e3
Tianlin Shi <tianlins@stanford.edu>
2015-11-02 20:19:35 -0800

    codelab scripts for pacman-v0

--- pacman-v0/test_Qlearn.py
+++ pacman-v0/test_Qlearn.py
@@ -9,48 +9,56 @@ import pyrl.agents.arch as arch
 from pyrl.algorithms.valueiter import DeepQlearn
 from pyrl.agents.agent import DQN
 from pyrl.evaluate import reward_stochastic
+from pyrl.layers import Conv, FullyConnected
 import theano
 import theano.tensor as T
+import os
+import cPickle as pickle
 
-_layout = layout.getLayout('pyrl/tasks/pacman/layouts/smallGrid.lay')
+_layout = layout.getLayout(os.environ['layout'])
 ghostType = DirectionalGhost
 agents = [ghostType( i+1 ) for i in range(2)]
-#display = graphicsDisplay.PacmanGraphics(zoom=1.0, frameTime = 0.1)
-# display = textDisplay.PacmanGraphics()
 display = textDisplay.NullGraphics()
 task = PacmanTask(_layout, agents, display)
-
-with Timer("random-play"):
-    scores = []
-    for it in range(1000):
-        overall_reward = 0.
-        if not task.is_end():
-            actions = task.valid_actions
-            action = choice(actions, 1)[0]
-            reward = task.step(action)
-            overall_reward += reward
-        else:
-            sys.stdout.flush()
-            _layout = layout.getLayout('pyrl/tasks/pacman/layouts/smallGrid.lay')
-            ghostType = DirectionalGhost
-            agents = [ghostType( i+1 ) for i in range(2)]
-            #display = graphicsDisplay.PacmanGraphics(zoom=1.0, frameTime = 0.1)
-            # display = textDisplay.PacmanGraphics()
-            display = textDisplay.NullGraphics()
-            task = PacmanTask(_layout, agents, display)
-
-
-input_dim = task.state_shape
+state_shape = task.state_shape
 
 def two_layer(states):
+    '''
+    two layer neural network with same number of hidden units.
+    '''
     state_vecs = states.flatten(2)
     return arch.two_layer(state_vecs, np.prod(task.state_shape), 128, task.num_actions)
 
-dqn = DQN(task, arch_func=two_layer, state_type=T.tensor4)
+def conv_net(states):
+    '''
+    conv+max-pool layer first followed by a fully-connected layer.
+    '''
+    conv1 = Conv(state_shape[0], output_dim=32, filter_size=(2,2), pool_size=(2,2), activation='relu')
+    h1 = conv1(states)
+    h1_dim = int((state_shape[1]-2+1)/2) * int((state_shape[2]-2+1)/2) * 32
+    h1 = h1.flatten(2)
+    fc1 = FullyConnected(input_dim=h1_dim, output_dim=128, activation='relu')
+    h2 = fc1(h1)
+    linear_layer = FullyConnected(input_dim=128, output_dim=task.num_actions, activation=None)
+    output = linear_layer(h2)
+    return (output, {
+        'conv1': conv1,
+        'fc1': fc1
+    })
+
+arch_func = globals()[os.environ['arch']]
+
+dqn = DQN(task, arch_func=arch_func, state_type=T.tensor4)
 learner = DeepQlearn(dqn, gamma=0.95, lr=1e-3, memory_size = 100, epsilon=0.5)
 scores = []
 for it in range(1000):
     learner.run(task, num_episodes = 100)
-    score = reward_stochastic(dqn, task, gamma=0.95, num_trials=10, tol=1e-4)
-    scores.append(score)
-    print 'it', it, 'score', scores[-1]
+    with Timer('iteration %d' % it):
+        score = reward_stochastic(dqn, task, gamma=0.95, num_trials=10, tol=1e-4)
+        scores.append(score)
+        print 'score', scores[-1]
+
+with open(os.environ['output'], 'w') as result:
+    pickle.dump({
+        'scores': scores
+    }, result)
